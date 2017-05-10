2524fce7b2f6746f074c4e94e9fa55190027ade2
Karl Kyck <karlkyck@gmail.com>
2017-05-10 17:39:31 +0100

    Completed async exercises.

--- app/async.js
+++ app/async.js
@@ -1,11 +1,28 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
+    async: function (value) {
+        return new Promise((resolve, reject) => resolve(value));
+    },
 
-  },
+    manipulateRemoteData: function (url) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", url);
 
-  manipulateRemoteData: function(url) {
+            xhr.onload = () => {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    resolve(xhr.response);
+                } else {
+                    reject(xhr.statusText);
+                }
+            };
 
-  }
+            xhr.onerror = () => reject(xhr.statusText);
+            xhr.send();
+        })
+            .then(response => JSON.parse(response))
+            .then(json => json.people.map(person => person.name))
+            .then(people => people.sort());
+    }
 };
