70c283a0c4568028d2b34f75d7b23ff6ec4b940d
Karl Kyck <karlkyck@gmail.com>
2017-05-15 20:19:37 +0100

    Completed count exercise.

--- app/count.js
+++ app/count.js
@@ -1,11 +1,30 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
-  count: function (start, end) {
-    const recCount = (start, end, currIndex) => {
+    count: function (start, end) {
+        const Cancel = function () {
+            this.cancelled = false;
 
-    };
+            this.cancel = () => {
+                this.cancelled = true;
+            };
 
+            this.isCancelled = () => {
+                return this.cancelled;
+            };
+        };
 
-  }
+        const cancel = new Cancel();
+
+        const recCount = (start, end, currIndex) => {
+            if (!cancel.isCancelled() && currIndex <= end) {
+                console.log(currIndex);
+                setTimeout(() => recCount(start, end, currIndex + 1), 100);
+            }
+        };
+
+        recCount(start, end, start);
+
+        return cancel;
+    }
 };
